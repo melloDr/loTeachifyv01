b50d58081b5b8ac7351a3ec29d65e0c68d577af9
melloDr <thanhlong020920@gmail.com>
2024-06-03 12:40:09 +0700

    Test commit

--- Teachify/Models/Item.cs
+++ Teachify/Models/Item.cs
@@ -6,6 +6,6 @@ namespace Teachify.Models
     {
         public string Id { get; set; }
         public string Text { get; set; }
-        public string Description { get; set; }
+        public string Description { get; set; } //hihi
     }
 }
